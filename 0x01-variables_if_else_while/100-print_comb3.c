9a210b6aa33ef79a565ba8fdfda7485b6f41ec2c
unknown <aklileseyoum294@gmail.com>
2022-03-12 05:41:05 +0300

    10. Inventing is a combination of brains and materials. The mor brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j = 'i + 1'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i = '8' && j = '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
