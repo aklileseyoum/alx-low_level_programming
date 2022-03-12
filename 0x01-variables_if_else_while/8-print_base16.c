cad6289ad4ddd4dc6fd51f2e43feb7a4786c9d62
unknown <aklileseyoum294@gmail.com>
2022-03-12 04:21:46 +0300

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for(c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
+
+
