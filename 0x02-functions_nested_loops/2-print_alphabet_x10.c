4718e9a7336329a6abccad8f95102b9a33acadf0
unknown <aklileseyoum294@gmail.com>
2022-03-16 02:39:56 +0300

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * _isalpha - tests whether a character is from the English alphabet.
+ * print_alphabet_x10 - prints all lower case letters 10 times
  * @c: character to be checked.
- * Return: 1 if the character is an English character.
- * 0 if the character is not an English character.
+ * Return: void
+ * 
  */
 void print_alphabet_x10(void)
 {
 	int i, j;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
