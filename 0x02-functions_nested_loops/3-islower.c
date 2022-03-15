c7781521d3c3cccf1470b26f242bf81232a3de6c
unknown <aklileseyoum294@gmail.com>
2022-03-16 00:37:51 +0300

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - returns integer
+ *
+ * Description - returns 0 if c is lower
+ */
+int _islower(int c)
+{
+	if (_islower('c'))
+		return (1);
+	else
+		return (0);
+}
