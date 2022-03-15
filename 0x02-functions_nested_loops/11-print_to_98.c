4a86c71ca140aa2ad18f04275cd3b6d347372e1d
unknown <aklileseyoum294@gmail.com>
2022-03-16 02:23:09 +0300

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,16 +5,14 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		int i;
-		for (i = n; i <=98; i++)
-			printf("%d, ", i);
+		for (; n <=98; n++)
+			printf("%d, ", n);
 	}
 	else if (n == 98)
 		printf("%d", n);
 	else
 	{
-		int i;
-		for (i = n; i >=98; i--)
-			printf("%d, ", i);
+		for (; n >=98; n--)
+			printf("%d, ", n);
 	}
 }
