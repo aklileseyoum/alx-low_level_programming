a9371d22bda604e2ce5cfa0432783729f6095e72
unknown <aklileseyoum294@gmail.com>
2022-03-21 23:51:23 +0300

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * _strncpy - copies a string up to n
+ * *_strcpy - copies the string 
  * @dest: Destination of the pointer to the string
  * @src: Source of the pointer to the string to copy
- * @n: Number of bytes to copy.
  * Return: Pointer to the destination string.
  */
 char *_strcpy(char *dest, char *src)
