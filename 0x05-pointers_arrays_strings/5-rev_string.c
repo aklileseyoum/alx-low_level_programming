21c49a1d69b44788dcef0b67346cd726c5108b58
unknown <aklileseyoum294@gmail.com>
2022-03-21 15:47:10 +0300

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,14 @@ void rev_string(char *s)
 	int len, i, j, k, temp;
 
         i = 0;
-        while(s[i] != '\0')
+        while (s[i] != '\0')
         {
                 i++;
         }
 
         len = i;
-	k = len - 1
-        for(i = len - 1; i >= 0; i--)
+	k = len - 1;
+        for (i = len - 1; i >= 0; i--)
         {
 		while (k > 0)
 		{
