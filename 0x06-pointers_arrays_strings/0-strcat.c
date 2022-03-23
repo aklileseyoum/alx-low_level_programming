9c374eb2a54180594cfccf891875e16ad838647a
root <root@Ak.localdomain>
2022-03-23 10:44:00 +0300

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	char new[1000];
+
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{ 
+		new[j] = dest[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		new[j] = src[i];
+		i++;
+		j++;
+	}
+	new[j] = '\0';
+
+	return (new);
+} 
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme for advanced pointers, arrays and strings
