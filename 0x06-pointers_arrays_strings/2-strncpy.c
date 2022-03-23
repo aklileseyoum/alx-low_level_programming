6583b1026ec7ff674525360a1e0c3781c6ee53d3
root <root@Ak.localdomain>
2022-03-23 12:06:49 +0300

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	j = 0;
+	while (src[j] = '\0')
+		j++;
+	if (j < n)
+	{
+		for (i = 0; i <= n; i++)
+			dest[i] = src [i];
+		dest[i] = '\0';
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+			dest[i] = src[i];
+	}
+
+	return (dest);
+}
