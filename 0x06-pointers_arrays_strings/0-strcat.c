5f49832f9414b5c8446a3088787471012e524ac8
unknown <aklileseyoum294@gmail.com>
2022-03-24 17:45:47 +0300

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,24 +3,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	char new[1000];
-
+	
 	i = 0;
 	j = 0;
 	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
 	{
-		new[j] = dest[i];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	i = 0;
-	while (src[i] != '\0')
-	{
-		new[j] = src[i];
-		i++;
-		j++;
-	}
-	new[j] = '\0';
+	dest[i] = '\0';
 
-	return (new);
+	return (dest);
 }
