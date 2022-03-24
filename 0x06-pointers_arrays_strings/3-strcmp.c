7380ab2f791dfb1b78278f8bebb1309e7e79a41b
unknown <aklileseyoum294@gmail.com>
2022-03-24 22:27:56 +0300

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,17 +7,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+        int i;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] == s2[i])
-			continue;
-		else
-			break;
-		i++;
-	}
+        i = 0;
+        while (s1[i] != '\0' && s2[i] != '\0')
+        {
+                if (s1[i] == s2[i])
+                        continue;
+                else
+                        break;
+                i++;
+        }
 
-	return (s1[i] - s2[i]);
+        return (s1[i] - s2[i]);
 }
