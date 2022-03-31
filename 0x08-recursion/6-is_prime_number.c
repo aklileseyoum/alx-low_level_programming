501f0c7c82db7229fb0a6d1fae2915f773464242
unknown <aklileseyoum294@gmail.com>
2022-03-31 05:29:36 +0300

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,30 @@
 #include "main.h"
-
+/**
+ * prime - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, 0 is returned, and errno is set appropriately.
+ */
 int prime(int a, int b)
 {
-	if (a == b)
+	if (a == b || a == -(b))
 		return (prime(a, (b + 1)));
 	if (b > 9)
 		return (0);
 	if (a % b == 0)
 		return (1);
+
 	return (prime(a, (b + 1)));
 }
-
+/**
+ * is_prime_number - returns 0 if its prime and 1 if its not
+ * @n: number
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int is_prime_number(int n)
 {
 	return(prime(n, 1));
