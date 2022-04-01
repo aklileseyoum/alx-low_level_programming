e0d95b07410e1b6fd04920a2a38b2dc88187c634
unknown <aklileseyoum294@gmail.com>
2022-04-02 01:12:14 +0300

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
+ */
 int main(int argc, char *argv[])
 {
 	int i = 1;
 
 	if (argc < 3)
+	{
 		printf("%s\n", "Error");
+		return (1);
+	}
 	else
 	{
-		while (argc <= 3)
+		while (argc > 1)
 		{
 			i = i * atoi(argv[argc]);
 			argc--;
@@ -19,4 +27,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
