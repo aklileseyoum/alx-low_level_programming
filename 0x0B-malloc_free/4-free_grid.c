ccd25e85bb86e40a68aab2573aefb7e34e866116
unknown <aklileseyoum294@gmail.com>
2022-04-05 22:25:53 +0300

    4. Its not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * free_grid - Entry point
+ *@grid: rows of matrix
+ *@height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
