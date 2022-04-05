a5b1678a0e668c6b60d36aceac05b04ae6f6c4de
unknown <aklileseyoum294@gmail.com>
2022-04-05 21:45:37 +0300

    3. If you even dream of beating me youd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+{
+	int **num;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	num = (int *)malloc(width * height * sizeof(int));
+	if (num == NULL)
+		return (NULL);
+	for (i = 0; i < width * height; i++)
+		num[i] = 0;
+
+	return (num);
+}
