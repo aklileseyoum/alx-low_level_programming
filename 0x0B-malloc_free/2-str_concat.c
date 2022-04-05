c443b5262d3d860033420233e977f9dc5dc07244
unknown <aklileseyoum294@gmail.com>
2022-04-05 20:54:24 +0300

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * str_concat - Entry point
+ *@s1: string 1
+ *@s2: string 2
+ * Return: pointer should point to a newly allocated space in memory or NULL
+ */
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
