d0db71c007dedab65c297ffaeabfb9f830a0faeb
unknown <aklileseyoum294@gmail.com>
2022-04-05 19:48:25 +0300

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * _strdup - Entry point
+ *@str: input string
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
 char *_strdup(char *str)
 {
 	char *s;
@@ -8,10 +13,11 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	s = (char *)malloc(sizeof(str) * sizeof(char));
+	for (i = 0; str[i] != '\0'; i++);
+	s = (char *)malloc(i + 1 * sizeof(char));
 	if (s != NULL)
 	{
-		for (i = 0; i < sizeof(str) * sizeof(s); i++)
+		for (i = 0; str[i] != '\0'; i++)
 			s[i] = str[i];
 		return (s);
 	}
