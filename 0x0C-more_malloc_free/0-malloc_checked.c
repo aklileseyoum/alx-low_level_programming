ed82238281af035be66a0634224f7407a5a75b2e
root <root@Ak.localdomain>
2022-04-07 20:02:35 +0300

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * malloc_checked - returns pointer which memory is allocated
- * @b - input
- * Return - output
+ * @b: input
+ * Return: output
  */
 void *malloc_checked(unsigned int b)
 {
 	void *p = (void *)malloc(b);
+
 	if (p == NULL)
 		exit(98);
 
