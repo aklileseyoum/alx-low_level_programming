fdce060d2f2ff01a376ad0d915eb9743b6fdb375
unknown <aklileseyoum294@gmail.com>
2022-04-07 19:54:18 +0300

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * malloc_checked - returns pointer which memory is allocated
+ * @b - unsigned integer which is the size of the memory in bytes
+ * Return - void pointer
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p = (void *)malloc(b);
