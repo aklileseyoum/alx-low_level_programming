52b799b331cd9fc06381eb65a2a1f484d1087381
unknown <aklileseyoum294@gmail.com>
2022-04-07 22:10:36 +0300

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,13 +9,15 @@
  */
 int *array_range(int min, int max)
 {
-	int *p;
+	int *p, i;
 
 	if (min > max)
 		return (NULL);
-	p = (int *)malloc((max - min) * sizeof(int));
+	p = (int *)malloc((max - min + 1) * sizeof(int));
 	if (p == NULL)
 		return (NULL);
+	for (i = 0; min <= max; i++)
+		p[i] = min++;
 
 	return (p);
 }
