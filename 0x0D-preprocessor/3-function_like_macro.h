3144f55fd4db87a6d2b6886bfa32c57844569f62
unknown <aklileseyoum294@gmail.com>
2022-04-11 22:29:08 +0300

    4. Sum

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef _3-function_like_macro_H
+#define _3-function_like_macro_H
+
+#define ABS(x) (x < 0)?(-1 * x):(x)
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifdef _4-sum_H
+#define _4-sum_H
+
+#define SUM(x, y) ((x) + (y))
+#endif
