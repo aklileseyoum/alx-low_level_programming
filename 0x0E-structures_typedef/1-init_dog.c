db12ebdc11ceead2a25e1f130944ce5076ee8e23
root <root@Ak.localdomain>
2022-04-12 01:20:47 +0300

    1. A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,9 @@
 #include "dog.h"
 #include <stdlib.h>
-/** 
+/**
  * init_dog - assigns value to the variables
  * @d: pointer that points to the struct dog
- * @name: name value 
+ * @name: name value
  * @age: age value
  * @owner: owner name valu
  * Return - nothing
