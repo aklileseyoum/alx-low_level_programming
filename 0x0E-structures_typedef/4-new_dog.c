073cf2dfcbe7132f38f2a5a1a7798087dc8fa497
root <root@Ak.localdomain>
2022-04-12 01:26:46 +0300

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,7 +5,7 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * Return - the new dog
+ * Return - pointer
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
