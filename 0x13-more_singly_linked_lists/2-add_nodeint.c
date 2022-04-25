132a2889b996ca6e0929bb1f593896e971e15f54
root <root@Ak.localdomain>
2022-04-26 02:05:34 +0300

    2. add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,8 +4,8 @@
 /**
  * add_nodeint - adds a node to the beginning of a linked list
  * @head: linked list pointer
- * @n: new element to be entered 
- * Return: address of the new element 
+ * @n: new element to be entered
+ * Return: address of the new element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
