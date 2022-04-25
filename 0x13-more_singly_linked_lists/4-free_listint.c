388f7ff62b5d9224858d5d1f97ed890fb8935efd
unknown <aklileseyoum294@gmail.com>
2022-04-26 02:35:30 +0300

    4. free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,11 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
-
-	for (temp = head; temp != NULL; temp = temp->next)
+	while (head)
 	{
-		free(temp->n);
-		free(temp);
+		listint_t *tmp = head;
+
+		head = head->next;
+		free(tmp);
 	}
 }
