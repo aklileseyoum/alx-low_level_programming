ed80fbce5af3959779019f061a4e40b07909b2ab
unknown <aklileseyoum294@gmail.com>
2022-04-26 21:09:37 +0300

    commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 /** 
  * free_listint2 - frees the list and sets head to null
  * @head: input list pointer
@@ -8,13 +9,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *h;
 	
-	temp = head;
+	temp = *head;
+	h = temp;
 	while (temp != NULL)
 	{
 		free(temp->n);
 		free(temp);
-		temp = temp->next;
+		h = h->next;
 	}
 	head = NULL;
 }
