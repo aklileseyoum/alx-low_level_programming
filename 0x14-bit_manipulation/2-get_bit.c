a77de27c2e33a40e8f6793d1f2f07a62b3b7d3be
unknown <aklileseyoum294@gmail.com>
2022-04-29 00:40:42 +0300

    2. 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,28 +1,18 @@
 #include "main.h"
-
+/**
+ * get_bit - returns bit according to index
+ * @n: input integer
+ * @index: input index
+ * Return: bit on index
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int *a, i = 0, j = 0, temp;
-	int num;
-
-	a = malloc(100 * sizeof(int));
-	if (a == NULL)
-		return (-1);
-	while (n != 0)
+	if (index > 64)
 	{
-		a[i] = n & 1;
-		n >>= 1;
-		i++;
-	}
-	if (index >= i)
 		return (-1);
-	for (j = 0; j < i; j++)
-	{
-		temp = a[j];
-		a[j] = a[i - 1];
-		a[i - 1] = temp;
-		i--;
 	}
-	
-	return (a[index]);
+	else
+	{
+		return (n >> index & 1);
+	}
 }
