913dbf69b2cd3ec4525ac81c2ad4efd1c6a3072b
unknown <aklileseyoum294@gmail.com>
2022-04-29 01:25:22 +0300

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,17 +6,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, sum;
+	unsigned int i, base = 0;
 
-	for (sum = 0, i = 0; b[i] != '\0'; i++)
+	if (b == NULL)
+		return (0);
+	for (i = 0 ; b[i] ; i++)
 	{
-		if (b[i] == '1')
-			sum = (sum << 1) | 1;
-		else if (b[i] == '0')
-			sum <<= 1;
-		else if (b[i] != '\0' && b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		base = base * 2 + b[i] - '0';
 	}
-
-	return (sum);
+	return (base);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,7 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int num = (n & 1);
+	int num = ((n & 1) + '0');
 
 	if (n > 1)
 	{
