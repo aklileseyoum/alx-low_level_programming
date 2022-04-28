8fd31329807c935264bfadaf12baa72c3ff3a2dc
unknown <aklileseyoum294@gmail.com>
2022-04-28 23:03:26 +0300

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,21 @@
 #include "main.h"
-#include <math.h>
-
+/**
+ * binary_to_uint - changes decimal to binary
+ * @b: input string of 0 and 1
+ * Return: binary number
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, mult, sum = 0;
+	unsigned int i, sum;
 
-	for (i = 0; i < sizeof(b); i++)
+	for (d = 0, i = 0; b[i] != '\0'; i++)
 	{
-		mult = b[i] * (pow(2, i));
-		sum += mult;
+		if (b[i] == '1')
+			sum = (sum<<1) | 1;
+		else if (b[i] == '0')
+			sum <<=1;
+		else if (b[i] != '\0' && b[i] != '1')
+			return (0);
 	}
 
 	return (sum);
