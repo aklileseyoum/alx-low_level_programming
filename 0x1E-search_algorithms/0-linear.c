e95063eb3dd080ece1d786039ed6fce674575fca
unknown <aklileseyoum294@gmail.com>
2022-10-05 23:00:21 +0300

    0. Linear Search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value)
+{
+	if(*array == NULL)
+		return (-1)
+	unsigned int i;
+	int found = 0;
+	for (i = 0; i < size; i++)
+	{
+		if (value == *(array + i))
+		{
+			found = 1;
+			return (i);
+		}
+	}
+
+	if (found == 0)
+		return (-1);
+}
